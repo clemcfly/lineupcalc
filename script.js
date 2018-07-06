12b038062f923174695f54790f724183bb6abb7d
clemcfly <lopez.clmnt@protonmail.com>
2018-07-06 15:34:30 +0200

    suppresion fichier js + correction bugs

--- script.js
+++
@@ -1,7 +0,0 @@
-<script>  M.AutoInit(); </script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var elems = document.querySelectorAll('.modal');
-        var instances = M.Modal.init(elems, options);
-    });
-    </script>
